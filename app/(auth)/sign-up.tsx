25705be2de8b6a297b16c35a6b9918080f6f7fe4
murrey-boop <murrithimaina@gmail.com>
2026-04-03 15:58:01 +0300

    sign up import update

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,6 +1,5 @@
-import { View, Text } from 'react-native'
-import React from 'react'
 import { Link } from 'expo-router'
+import { Text, View } from 'react-native'
 
 const signUp = () => {
   return (
