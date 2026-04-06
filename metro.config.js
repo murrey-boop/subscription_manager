6d3d6865f633b99bbecacd96a8238c530813fe38
murrey-boop <murrithimaina@gmail.com>
2026-04-06 15:41:30 +0300

    Fix EAS ci lockfile sync and metro watcher warning

--- metro.config.js
+++ metro.config.js
@@ -3,5 +3,10 @@ const { withNativewind } = require("nativewind/metro");
  
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
+// Strip deprecated watcher option to avoid EAS/RN config validation warnings.
+if (config.watcher && "unstable_workerThreads" in config.watcher) {
+	delete config.watcher.unstable_workerThreads;
+}
  
 module.exports = withNativewind(config);
